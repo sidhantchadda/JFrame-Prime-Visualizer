ee9b660c26fd4e14dcbb93e7bacf4e16ee15f000
Sidhant Chadda <schadda.sj@mysummitps.org>
2014-10-02 21:20:00 -0700

    Cleaned up Code

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,20 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	private static int height =100;
+	private static int width = 100;
+
 	public static void main(String[] args) {
+		// fixes bugs in JDK.
+		System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+		
         Runnable r = new Runnable() {
             public void run() {
-                // the GUI as seen by the user (without frame)
-                // A single component added to a GBL with no constraint
-                // will be centered.
-            	int height =100;
-            	int width = 100;
                 JPanel gui = new JPanel(new GridLayout(height, width));
-
-                //gui.setBackground(Color.WHITE);
-
-                SquarePanel p = new SquarePanel();
-                p.setBackground(Color.RED);
-
+                
+                // iterates through all the numbers in the grid and checks if prime
+                // if a number is prime its associated square becomes Blue.
+                // if a number is not prime its square becomes Dark Gray.
                 for(int x = 0; x<height*width; x++)	{
                 	SquarePanel sp = new SquarePanel();
                 	sp.setBorder(BorderFactory.createLineBorder(Color.black, 1));
@@ -37,17 +36,13 @@ public class Main {
                 	}
                 	gui.add(sp);
                 }
-               // gui.add(p);
 
-                JFrame f = new JFrame("Demo");
+                JFrame f = new JFrame("Grid");
+                // exits program when the window is closed.
                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 f.add(gui);
-                // Ensures JVM closes after frame(s) closed and
-                // all non-daemon threads are finished
-                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                // See http://stackoverflow.com/a/7143398/418556 for demo.
-                f.setLocationByPlatform(true);
-
+                // sets the location of the frame in the middle of the screen.
+                f.setLocationRelativeTo(null);
                 // ensures the frame is the minimum size it needs to be
                 // in order display the components within it
                 f.pack();
@@ -58,10 +53,10 @@ public class Main {
             }
         };
         // Swing GUIs should be created and updated on the EDT
-        // http://docs.oracle.com/javase/tutorial/uiswing/concurrency/initial.html
         SwingUtilities.invokeLater(r);
     }
 
+	// Detects if x is Prime.
 	private static boolean isPrime(int x) {
 		if(x <= 1)	{
 			return false;
--- src/SquarePanel.java
+++ src/SquarePanel.java
@@ -3,8 +3,9 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-
+@SuppressWarnings("serial")
 public class SquarePanel extends JPanel{
+	// defining of a square in JPanel
 	@Override
     public Dimension getPreferredSize() {
         Dimension d = super.getPreferredSize();
